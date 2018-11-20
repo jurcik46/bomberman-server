08c7206fb540dbdf23e559ee2b602d4951ff9fb7
Ján Jurč <jurcik46@gmail.com>
2018-11-20 21:52:45 +0100

    Merge branch 'develop' into feature/tcp_to_udp
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -10,8 +10,8 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include "logging/log.h"
-void error(const char *msg)
-{
+
+void error(const char *msg) {
     perror(msg);
     exit(0);
 }
@@ -59,18 +59,18 @@ int main(int argc, char *argv[]) {
     if (bind(sockfd, (struct sockaddr *) &serv_addr,
              sizeof(serv_addr)) < 0)
         error("ERROR on binding");
-    listen(sockfd,5);
+    listen(sockfd, 5);
     clilen = sizeof(cli_addr);
     newsockfd = accept(sockfd,
                        (struct sockaddr *) &cli_addr,
                        &clilen);
     if (newsockfd < 0)
         error("ERROR on accept");
-    bzero(buffer,256);
-    n = read(newsockfd,buffer,255);
+    bzero(buffer, 256);
+    n = read(newsockfd, buffer, 255);
     if (n < 0) error("ERROR reading from socket");
-    printf("Here is the message: %s\n",buffer);
-    n = write(newsockfd,"I got your message",18);
+    printf("Here is the message: %s\n", buffer);
+    n = write(newsockfd, "I got your message", 18);
     if (n < 0) error("ERROR writing to socket");
     close(newsockfd);
     close(sockfd);
